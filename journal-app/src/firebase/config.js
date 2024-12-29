653f4697e28b3a6267d1a7cf7dabdc64cf4bc0d3
Andrés Sosa <136858623+Andrewsy1004@users.noreply.github.com>
2024-12-29 15:50:44 +0000

    Update config.js

--- journal-app/src/firebase/config.js
+++ journal-app/src/firebase/config.js
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyUhtbnrkjOwB-aswONCPe-tiUqhITRo4",
-  authDomain: "react-course-d7694.firebaseapp.com",
-  projectId: "react-course-d7694",
-  storageBucket: "react-course-d7694.appspot.com",
-  messagingSenderId: "446496076835",
-  appId: "1:446496076835:web:a361d75b0c5cf52a756d96",
-  measurementId: "G-LWSVYH5NKM"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
